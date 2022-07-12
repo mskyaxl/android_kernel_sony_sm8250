36c462e3b355d3e37f76713cd006b91300321bb0
Luofan Chen <clfbbn@gmail.com>
2022-07-13 00:24:16 +0800

    build.sh: update
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ CLANG_PATH=/home/bbn/LineageOS/LineageOS/prebuilts/clang/host/linux-x86/clang-r3
 BUILD_CROSS_COMPILE=${GCC_PATH}/aarch64-linux-android-
 CLANG_TRIPLE=aarch64-linux-android-
 
-DEFCONFIG=vendor/kona-perf_defconfig
+DEFCONFIG=pdx203-bbn_defconfig
 
-export KBUILD_DIFFCONFIG=pdx203_diffconfig
+# export KBUILD_DIFFCONFIG=pdx203_diffconfig
 export PATH=${CLANG_PATH}:${PATH}
 
-# make -j56 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
-#     CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} \
-#     CROSS_COMPILE=$BUILD_CROSS_COMPILE $DEFCONFIG
+make -j56 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
+    CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} \
+    CROSS_COMPILE=$BUILD_CROSS_COMPILE $DEFCONFIG
 
 make -j56 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
     CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} \
